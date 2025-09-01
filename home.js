fbe845beb89d3698591519414b78b3ec5a765d37
khairul965 <khairulrakib79@gmail.com>
2025-09-01 13:41:57 +0600

    2nd commit

--- home.js
+++ home.js
@@ -49,7 +49,7 @@ const totalAmount = availableAmount - Amount
 document.getElementById("available-money").innerText = totalAmount
 })
 
-//function to toggle
+//function to toggle-1
 
 function handletoggle(id){
      const forms =document.getElementsByClassName("form")
@@ -61,28 +61,48 @@ function handletoggle(id){
     document.getElementById(id).style.display = "block"
 }
 
+//function to toggle-2
+
+function buttonToggle(id){
+    const formsBtn =document.getElementsByClassName("form-btn")
+
+   for(const btn of formsBtn){
+    btn.classList.remove("border-[#0874f2]","bg-[#0874f20d]")
+    btn.classList.add("border-gray-300")
+   }
+
+   document.getElementById(id ).classList.remove("border-gray-300")
+   document.getElementById(id).classList.add("border-[#0874f2]","bg-[#0874f20d]")
+}
+
 
 //Toggling feature
 
 document.getElementById("add-button").addEventListener("click",function(){
    handletoggle("add-money-parent")
+   buttonToggle("add-button")
 })
 
 document.getElementById("cash-button").addEventListener("click",function(){
     handletoggle("cash-out-parent")
+    buttonToggle("cash-button")
+    
 })
 
 document.getElementById("transfer-button").addEventListener("click",function(){
     handletoggle("transfer-parent")
-
+    buttonToggle("transfer-button")
 })
 
 document.getElementById("bonus-button").addEventListener("click",function(){
     handletoggle("bonus-parent")
+    buttonToggle("bonus-button")
 })
 
 document.getElementById("bill-button").addEventListener("click",function(){
     handletoggle("bill-parent")
+    buttonToggle("bill-button")
+   
 })
 
 document.getElementById("cash-button").addEventListener("click",function(){
